a1e04b1bce49bd2866db94d368288023a0c17501
BugHunter-X <askzenai@gmail.com>
2024-12-12 08:22:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+The corrected import statement uses curly braces to import the 'MyComponent' component. This is the correct way to import named exports in React Native.  See the change between bug.js and bugSolution.js for the fix.
